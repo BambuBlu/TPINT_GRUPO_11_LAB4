6d6a374da93519a6c3c889e0e600c6055c9737be
coutnico <nicoo_rodrii@hotmail.com>
2024-11-14 19:49:49 -0300

    Feat: Change logic on login.

--- src/integrador/servlets/ServletLogin.java
+++ src/integrador/servlets/ServletLogin.java
@@ -8,9 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import integrador.model.Usuario;
+import integrador.negocio.UsuarioNegocio;
+import integrador.negocioimpl.UsuarioNegocioImpl;
 import integrador.enums.Roles;
 import integrador.model.Cliente;
-
 
 /**
  * Servlet implementation class ServletLogin
@@ -18,66 +19,69 @@ import integrador.model.Cliente;
 @WebServlet("/ServletLogin")
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletLogin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletLogin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		if (request.getParameter("btnInicioSesion") != null) {
 			IniciarSesionUsuario(request, response);
 		}
 	}
-	
-	public void IniciarSesionUsuario(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void IniciarSesionUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String nombreUsuario = request.getParameter("txtNombre");
 		String contraseniaUsuario = request.getParameter("txtContrasenia");
-		
-		//Validacion de valores nulos o vacios 
-		if ((nombreUsuario != null || nombreUsuario != "") && (contraseniaUsuario != null || contraseniaUsuario != "")) {
-			//Logica con la base de datos para verificar si existe y esta hablitado de pasar el filtro de seguridad, si todo esta en orden, se crea el objeto y se guarda en Session 
-			
-			Usuario usuario = new Usuario();
-			usuario.setNombreUsuario(nombreUsuario);
-			usuario.setContrasena(contraseniaUsuario);
-			usuario.setRol(Roles.ADMIN);
-			//por ahora para pasar el filtro, falta logica
-			usuario.setBaja(false);
-			
-			
-			usuario.setCliente(new Cliente ());
-			
-			if (!usuario.getBaja()) {
-				request.getSession().setAttribute("SessionActual", usuario);
-				if (usuario.getRol() == Roles.CLIENTE)
-					request.getRequestDispatcher("MainPage.jsp").forward(request, response); 
-				else if (usuario.getRol() == Roles.ADMIN)
-					request.getRequestDispatcher("AdminMainPage.jsp").forward(request, response); 
-					
-			}	
-		}
-		else {
-			request.setAttribute("SessionActual", "Por favor, complete todos los campos.");
-	        request.getRequestDispatcher("index.jsp").forward(request, response); 
-		}	
-	}
 
-	
+		// Validacion de valores nulos o vacios
+		if ((nombreUsuario != null || nombreUsuario != "")
+				&& (contraseniaUsuario != null || contraseniaUsuario != "")) {
+			// Logica con la base de datos para verificar si existe y esta hablitado de
+			// pasar el filtro de seguridad, si todo esta en orden, se crea el objeto y se
+			// guarda en Session
+
+			UsuarioNegocioImpl negocio = new UsuarioNegocioImpl();
+
+			for (Usuario usuario : negocio.GetAllUsuariosActivos()) {
+				if (usuario.getNombreUsuario().equals(nombreUsuario)
+						&& usuario.getContrasena().equals(contraseniaUsuario)) {
+					request.getSession().setAttribute("SessionActual", usuario);
+					if (usuario.getRol() == Roles.CLIENTE)
+						request.getRequestDispatcher("MainPage.jsp").forward(request, response);
+					else if (usuario.getRol() == Roles.ADMIN)
+						request.getRequestDispatcher("AdminMainPage.jsp").forward(request, response);
+				}
+			}
+
+			request.setAttribute("SessionActual", "Usuario inexistente, intentelo de nuevo...");
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+
+		} else {
+			request.setAttribute("SessionActual", "Por favor, complete todos los campos.");
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
+	}
 
 }
