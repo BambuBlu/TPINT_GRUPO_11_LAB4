f249c258822873b1aa52076b664d74aaf773a9d7
coutnico <nicoo_rodrii@hotmail.com>
2024-11-13 21:11:47 -0300

    Feat: Implement functionality in ClienteNegocio

--- src/integrador/negocio/ClienteNegocio.java
+++ src/integrador/negocio/ClienteNegocio.java
@@ -1,5 +1,14 @@
 package integrador.negocio;
 
-public class ClienteNegocio {
+import java.util.ArrayList;
 
+import integrador.model.Cliente;
+import integrador.model.Usuario;
+
+public interface ClienteNegocio {
+	
+	ArrayList<Cliente> GetAllClientes(String estado);
+	boolean CrearCliente(Cliente nuevoCliente, Usuario nuevoUsuario);
+	boolean ModificarCliente(Cliente clienteModificado, Usuario usuarioModificado);
+	boolean EliminarCliente(Cliente clienteAeliminar);
 }
--- src/integrador/negocioimpl/ClienteNegocioImpl.java
+++ src/integrador/negocioimpl/ClienteNegocioImpl.java
@@ -1,5 +1,71 @@
 package integrador.negocioimpl;
 
-public class ClienteNegocioImpl {
+import java.util.ArrayList;
+
+import integrador.daoimpl.ClienteDaoImpl;
+import integrador.model.Cliente;
+import integrador.model.Usuario;
+import integrador.negocio.ClienteNegocio;
+
+public class ClienteNegocioImpl implements ClienteNegocio {
+
+	private ClienteDaoImpl dao = new ClienteDaoImpl();
+
+	@Override
+	public ArrayList<Cliente> GetAllClientes(String estado) {
+		try {
+			ArrayList<Cliente> clientesFiltrados = new ArrayList<Cliente>();
+			for (Cliente cliente : dao.GetAllClientes()) {
+				if (cliente.getEstado() == estado)
+					clientesFiltrados.add(cliente);
+			}
+
+			return clientesFiltrados;
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+
+	@Override
+	public boolean CrearCliente(Cliente nuevoCliente, Usuario nuevoUsuario) {
+		try {
+			if (dao.CrearCliente(nuevoCliente, nuevoUsuario))
+				return true;
+			else
+				return false;		
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	@Override
+	public boolean ModificarCliente(Cliente clienteModificado, Usuario usuarioModificado) {
+		try {
+			if (dao.ModificarCliente(clienteModificado, usuarioModificado))
+				return true;
+			else
+				return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	@Override
+	public boolean EliminarCliente(Cliente clienteAeliminar) {
+		try {
+			
+		if (dao.EliminarCliente(clienteAeliminar))
+			return true;
+		else
+			return false;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
 
 }
